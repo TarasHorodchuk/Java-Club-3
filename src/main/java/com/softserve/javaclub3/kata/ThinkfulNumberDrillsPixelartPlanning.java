5708a996829a000eead8751fe919103ad0f56330
Sorbexv1 <Sorbexv1@gmail.com>
2021-10-05 20:17:06 +0300

    Task 4 added (with basic structure);

---
+++ src/main/java/com/softserve/javaclub3/kata/ThinkfulNumberDrillsPixelartPlanning.java
@@ -0,0 +1,8 @@
+package com.softserve.javaclub3.kata;
+
+public class ThinkfulNumberDrillsPixelartPlanning {
+
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return wallLength % pixelSize == 0;
+    }
+}
